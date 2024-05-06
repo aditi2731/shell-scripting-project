f637c68571eda29bd14e4640f09b924b4e022afc
Aditi Singh <142771205+aditi2731@users.noreply.github.com>
2024-05-06 19:29:33 +0530

    digi_watch.sh

---
+++ digi_watch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Red=$'\e[1;31m'
+Green=$'\e[1;32m'
+Blue=$'\e[1;34m'
+
+while true
+do
+        clear
+        echo $Blue $(date +%T)
+        sleep ls
+done
+
+  //To close the clock use ctrl+c                                                                                                                                          
+                                                                                                                                            for 
