5005075b5dce318659eb0f0573cd100e04122959
Aditi Singh <142771205+aditi2731@users.noreply.github.com>
2024-05-06 19:48:30 +0530

    file_system.sh

---
+++ file_system.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "File Installer"
+sleep 1
+echo "Enter project name"
+sleep 1
+read projectname
+mkdir $projectname #root folder
+cd $projectname
+mkdir file1
+mkdir file2
+mkdir file3
+mkdir file4
